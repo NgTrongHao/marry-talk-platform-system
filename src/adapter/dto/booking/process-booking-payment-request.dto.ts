f9b32ac7af8324c5ad62a60ea1254e4bf5fb0a7d
Nguyen Trong Hao <ngtronghao08@gmail.com>
2025-03-27 00:26:26 +0700

    fix: change URL validation to string type for returnUrl in payment DTO

--- src/adapter/dto/booking/process-booking-payment-request.dto.ts
+++ src/adapter/dto/booking/process-booking-payment-request.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class ProcessBookingPaymentDto {
   @ApiProperty({ example: 'https://your-return-url.com' })
   // @IsUrl({ require_tld: false })
+  @IsString()
   returnUrl: string;
 }
